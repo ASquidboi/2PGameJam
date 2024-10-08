a42f416808f9585172cbd882194dcc558c13d1c0
Squiddy <49382935+ASquidboi@users.noreply.github.com>
2024-10-08 16:18:49 -0700

    ruh roh raggy

--- Assets/P1Controller.cs
+++ Assets/P1Controller.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class P1Controller : MonoBehaviour
@@ -38,10 +38,44 @@ public class P1Controller : MonoBehaviour
     private void FixedUpdate()
     {
         //rb.velocity = new Vector2(Input.GetAxis("P1Horizontal") * MoveSpeed, Input.GetAxis("P1Vertical") * MoveSpeed);
-        Vector2 dir = new Vector2(Mathf.Cos(rb.rotation * Mathf.Deg2Rad), Mathf.Sin(rb.rotation * Mathf.Deg2Rad));
-        rb.velocity = dir.normalized * MoveSpeed;
+        /*Vector2 dir = new Vector2(Mathf.Cos(rb.rotation * Mathf.Deg2Rad), Mathf.Sin(rb.rotation * Mathf.Deg2Rad));
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = dir.normalized * MoveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = dir.normalized * -MoveSpeed;
+        } */
+
+        Vector2 forwardDir = new Vector2(Mathf.Cos(rb.rotation * Mathf.Deg2Rad), Mathf.Sin(rb.rotation * Mathf.Deg2Rad));
+        Vector2 rightDir = new Vector2(-forwardDir.y, forwardDir.x); // 90 degrees rotation for right
+
+        Vector2 desiredVelocity = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            desiredVelocity += forwardDir.normalized * MoveSpeed; // Move forward
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            desiredVelocity += -forwardDir.normalized * MoveSpeed; // Move backward
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            desiredVelocity += rightDir.normalized * MoveSpeed; // Strafe left
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            desiredVelocity += -rightDir.normalized * MoveSpeed; // Strafe right
+        }
+
+        rb.velocity = desiredVelocity;
+
+
         // Rotate counterclockwise with Q
-        
+
         //transform.rotation = Quaternion.Euler(0, 0, currentRotation);
         rb.rotation = currentRotation;
     }
--- Assets/P2Controller.cs
+++ Assets/P2Controller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class P2Controller : MonoBehaviour
@@ -8,7 +9,8 @@ public class P2Controller : MonoBehaviour
     [SerializeField] float MoveSpeed;
     [SerializeField] float turnSpeed = 200f; // Adjust for your desired turn speed
     private float currentRotation;
-    public float P2Health = 100f;
+    [SerializeField] public float P2Health = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,23 +20,69 @@ public class P2Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(Input.GetAxis("P2Horizontal") * MoveSpeed, Input.GetAxis("P2Vertical") * MoveSpeed);
-        // Rotate counterclockwise with Q
         if (Input.GetKey(KeyCode.U))
         {
             currentRotation += turnSpeed * Time.deltaTime;
+
         }
 
-        // Optionally, rotate clockwise with E
         if (Input.GetKey(KeyCode.O))
         {
             currentRotation -= turnSpeed * Time.deltaTime;
         }
-        transform.rotation = Quaternion.Euler(0, 0, currentRotation);
+
     }
+
+    private void FixedUpdate()
+    {
+        //rb.velocity = new Vector2(Input.GetAxis("P1Horizontal") * MoveSpeed, Input.GetAxis("P1Vertical") * MoveSpeed);
+        /*Vector2 dir = new Vector2(Mathf.Cos(rb.rotation * Mathf.Deg2Rad), Mathf.Sin(rb.rotation * Mathf.Deg2Rad));
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = dir.normalized * MoveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = dir.normalized * -MoveSpeed;
+        } */
+
+        Vector2 forwardDir = new Vector2(Mathf.Cos(rb.rotation * Mathf.Deg2Rad), Mathf.Sin(rb.rotation * Mathf.Deg2Rad));
+        Vector2 rightDir = new Vector2(-forwardDir.y, forwardDir.x); // 90 degrees rotation for right
+
+        Vector2 desiredVelocity = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.I))
+        {
+            desiredVelocity += forwardDir.normalized * MoveSpeed; // Move forward
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            desiredVelocity += -forwardDir.normalized * MoveSpeed; // Move backward
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            desiredVelocity += rightDir.normalized * MoveSpeed; // Strafe left
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            desiredVelocity += -rightDir.normalized * MoveSpeed; // Strafe right
+        }
+
+        rb.velocity = desiredVelocity;
+
+
+        // Rotate counterclockwise with Q
+
+        //transform.rotation = Quaternion.Euler(0, 0, currentRotation);
+        rb.rotation = currentRotation;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        P2Health -= damage;
+        //Damage effects & multipliers & stuff
+    }
+
 }
